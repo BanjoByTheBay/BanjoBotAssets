363e6812c42241e74bd49ee8643a215161d99c9c
Taradino C <4934394+taradinoc@users.noreply.github.com>
2023-01-13 16:50:18 -0800

    Updated mappings service URL

--- BanjoBotAssets/Config/ServiceCollectionExtensions.AppConfig.cs
+++ BanjoBotAssets/Config/ServiceCollectionExtensions.AppConfig.cs
@@ -177,7 +177,7 @@ namespace BanjoBotAssets.Extensions
             services.AddOptions<MappingsOptions>()
                 .Configure<IConfiguration>((options, config) =>
                 {
-                    options.MappingsApiUri = "https://fortnitecentral.gmatrixgames.ga/api/v1/mappings";
+                    options.MappingsApiUri = "https://fortnitecentral.genxgames.gg/api/v1/mappings";
                     options.LocalFilePath = "mappings.usmap";
                     config.GetSection(nameof(MappingsOptions)).Bind(options);
                 });
