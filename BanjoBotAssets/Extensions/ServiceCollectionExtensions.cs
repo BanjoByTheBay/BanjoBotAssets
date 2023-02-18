2e3f5887a2f076397515bea2c610eea4e414ad7b
Taradino C <4934394+taradinoc@users.noreply.github.com>
2023-02-17 21:01:01 -0800

    Added license headers and license document.

--- BanjoBotAssets/Extensions/ServiceCollectionExtensions.cs
+++ BanjoBotAssets/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,21 @@
-﻿using System.Reflection;
+﻿/* Copyright 2023 Tara "Dino" Cassatt
+ * 
+ * This file is part of BanjoBotAssets.
+ * 
+ * BanjoBotAssets is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BanjoBotAssets is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BanjoBotAssets.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System.Reflection;
 
 // TODO: use a source generator instead of ServiceCollectionExtensions.AddDerivedServices
 
