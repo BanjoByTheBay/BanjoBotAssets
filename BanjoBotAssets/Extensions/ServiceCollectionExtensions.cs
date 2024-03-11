1477226860a23861b7e9bd60a1cd0bf53ece7cbf
Taradino C. <4934394+taradinoc@users.noreply.github.com>
2024-03-11 13:52:49 -0700

    Removed SoloForTestingAttribute

--- BanjoBotAssets/Extensions/ServiceCollectionExtensions.cs
+++ BanjoBotAssets/Extensions/ServiceCollectionExtensions.cs
@@ -15,16 +15,12 @@
  * You should have received a copy of the GNU General Public License
  * along with BanjoBotAssets.  If not, see <http://www.gnu.org/licenses/>.
  */
-using System.Reflection;
-
 // TODO: use a source generator instead of ServiceCollectionExtensions.AddDerivedServices
 
 namespace BanjoBotAssets.Extensions
 {
     internal static partial class ServiceCollectionExtensions
     {
-        private static bool HasSoloAttribute(Type t) => t.GetCustomAttribute<SoloForTestingAttribute>(false) != null;
-
         public static IServiceCollection AddDerivedServices<TService, TImplementationBase>(this IServiceCollection services, ServiceLifetime lifetime)
             where TService : class
             where TImplementationBase : class
@@ -32,11 +28,6 @@ namespace BanjoBotAssets.Extensions
             var types = from t in typeof(TImplementationBase).Assembly.GetTypes()
                         where typeof(TImplementationBase).IsAssignableFrom(t) && t.IsClass && !t.IsAbstract
                         select t;
-
-            if (types.Any(HasSoloAttribute))
-            {
-                types = types.Where(HasSoloAttribute);
-            }
 
             foreach (var t in types)
             {
