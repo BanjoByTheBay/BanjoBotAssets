b87c8c71d093f18add330968902229b2a55a0cad
TomatechGames <theludwiggamer@gmail.com>
2024-04-10 04:12:47 +0100

    Added exporters for ConsumableAccountItem and Loadout (#52)

---
+++ BanjoBotAssets/Exporters/UObjects/ConsumableAccountItemExporter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanjoBotAssets.Exporters.UObjects
+{
+    internal sealed class ConsumableAccountItemExporter : UObjectExporter
+    {
+        public ConsumableAccountItemExporter(IExporterContext services) : base(services) { } 
+
+        protected override string Type => "ConsumableAccountItem";
+
+        protected override bool InterestedInAsset(string name)
+        {
+            bool interested = name.Contains("/AccountConsumables/", StringComparison.OrdinalIgnoreCase);
+
+            return interested;
+        }
+    }
+}
---
+++ BanjoBotAssets/Exporters/UObjects/LoadoutExporter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanjoBotAssets.Exporters.UObjects
+{
+    internal sealed class LoadoutExporter : UObjectExporter
+    {
+        public LoadoutExporter(IExporterContext services) : base(services) { } 
+
+        protected override string Type => "CampaignHeroLoadout";
+
+        protected override bool InterestedInAsset(string name)
+        {
+            bool interested = name.Contains("/Loadouts/", StringComparison.OrdinalIgnoreCase) && !name.Contains("/Slots/", StringComparison.OrdinalIgnoreCase);
+
+            return interested;
+        }
+    }
+}
