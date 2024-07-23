fff946add7e4c75c08d59201070cb899643b6523
TomatechGames <theludwiggamer@gmail.com>
2024-07-23 15:38:46 +0100

    Added copyright notice to AccoladeItemData

--- BanjoBotAssets.Json/AccoladeItemData.cs
+++ BanjoBotAssets.Json/AccoladeItemData.cs
@@ -1,9 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/* Copyright 2023 Tara "Dino" Cassatt
+ * 
+ * This file is part of BanjoBotAssets.
+ * 
+ * BanjoBotAssets is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BanjoBotAssets is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BanjoBotAssets.  If not, see <http://www.gnu.org/licenses/>.
+ */
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BanjoBotAssets.Json
 {
