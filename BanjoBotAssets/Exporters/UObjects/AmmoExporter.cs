7e11e94963f31b732bcb02412afd64ddf8cb13bf
TomatechGames <tomatechgames@gmail.com>
2025-01-18 19:51:24 +0000

    Export Ammo Items

---
+++ BanjoBotAssets/Exporters/UObjects/AmmoExporter.cs
@@ -0,0 +1,37 @@
+﻿/* Copyright 2023 Tara "Dino" Cassatt
+ * 
+ * This file is part of BanjoBotAssets.
+ * 
+ * BanjoBotAssets is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BanjoBotAssets is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BanjoBotAssets.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using CUE4Parse.FN.Structs.GA;
+
+namespace BanjoBotAssets.Exporters.UObjects
+{
+    internal sealed class AmmoExporter(IExporterContext services) : UObjectExporter<UObject, NamedItemData>(services)
+    {
+        protected override string Type => "Ammo";
+
+        protected override bool InterestedInAsset(string name)
+        {
+            if (name.Contains("/Athena/", StringComparison.OrdinalIgnoreCase))
+                return false;
+            if (name.Contains("/PrimalGameplay/", StringComparison.OrdinalIgnoreCase))
+                return false;
+
+            return name.Contains("/Items/Ammo/AmmoData", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
