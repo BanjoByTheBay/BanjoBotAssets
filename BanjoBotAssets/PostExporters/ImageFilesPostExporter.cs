87e8b38c25e790f00b2e57ab94d0336787bc0f41
TomatechGames <tomatechgames@gmail.com>
2025-07-27 11:26:48 +0100

    Fix DetexHelper not initialising correctly

--- BanjoBotAssets/PostExporters/ImageFilesPostExporter.cs
+++ BanjoBotAssets/PostExporters/ImageFilesPostExporter.cs
@@ -45,7 +45,7 @@ namespace BanjoBotAssets.PostExporters
 
             Directory.CreateDirectory(options.Value.OutputDirectory);
 
-            DetexHelper.Initialize(DetexHelper.DLL_NAME);
+            DetexHelper.Initialize("../external/CUE4Parse/CUE4Parse-Conversion/Resources/Detex.dll");
 
             foreach (var (imageType, wantExport) in options.Value.Type)
             {
